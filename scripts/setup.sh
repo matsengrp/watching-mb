dfeb9a85559b882d763ba9aefa5efe16fe4d37f9
Erick Matsen <ematsen@gmail.com>
2022-01-04 05:28:18 -0800

    setup fixes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,21 +2,24 @@
 
 set -eu
 
-mkdir -p analysis
-
+# A version of realpath that works on OSX, ensuring that the path has no
+# spaces.
 realpath_osx() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
 for i in $(seq 8);
 do
-    mkdir -p analysis/ds${i}
-    # Ensure current path has no spaces
-    data_path=analysis/ds${i}/data
-    test ! -e $data_path || ln -s $(realpath_osx ds-data/ds${i}) $data_path
+    for target in golden;
+    do
+        mkdir -p $target/ds${i}
+        data_path=$(realpath $target)/ds${i}/data
+        test -e $data_path || ln -s $(realpath ds-data/ds${i}) $data_path
+    done
 done
 
-for i in scripts/wtch-*;
+# Install all scripts with the `wtch` prefix into the conda environment.
+for script in scripts/wtch-*;
 do
-    ln -s $(realpath_osx $i) $CONDA_PREFIX/bin/
+    test -e $CONDA_PREFIX/bin/$(basename $script) || ln -s $(realpath_osx $script) $CONDA_PREFIX/bin/
 done
