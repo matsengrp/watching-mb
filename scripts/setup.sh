ef635b092ccd8bad9fce8872b696ddbba69b97df
Erick Matsen <ematsen@gmail.com>
2022-01-11 07:03:26 -0800

    getting dag stats of pickle

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,6 +8,12 @@ realpath_osx() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
+# Install all scripts with the `wtch` prefix into the conda environment.
+for script in scripts/wtch-*;
+do
+    test -e $CONDA_PREFIX/bin/$(basename $script) || ln -s $(realpath_osx $script) $CONDA_PREFIX/bin/
+done
+
 for i in 1 3 4 5 6 7 8;
 do
     for target in analysis golden;
@@ -17,10 +23,4 @@ do
         test -e $data_path || ln -s $(realpath ds-data/ds${i}) $data_path
     done
     ln -s $(realpath_osx golden/ds${i}) $(realpath_osx analysis/ds${i}/golden)
-done
-
-# Install all scripts with the `wtch` prefix into the conda environment.
-for script in scripts/wtch-*;
-do
-    test -e $CONDA_PREFIX/bin/$(basename $script) || ln -s $(realpath_osx $script) $CONDA_PREFIX/bin/
 done
