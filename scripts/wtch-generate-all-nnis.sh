3b41f9eae102005a2aba4c9727572ba1ebfb0618
Erick Matsen <ematsen@gmail.com>
2022-01-26 09:11:14 -0800

    rerooting as part of tree normalization

--- scripts/wtch-generate-all-nnis.sh
+++ scripts/wtch-generate-all-nnis.sh
@@ -3,17 +3,20 @@
 
 set -eu -o pipefail
 
-if test $# -ne 1; then
-    echo "Please supply a newick file of trees for which we can generate all NNI neighbors."
+if test $# -ne 2; then
+    echo "Please supply a newick file of trees for which we can generate all NNI neighbors, and the taxon id to use for rerooting."
     exit 1
 fi
+
+tree_path="$1"
+root_taxon="$2"
 
 temp_file=$(mktemp)
 trap "rm -f $temp_file" 0 2 3 15
 
-cat "$1" | while read line
+cat "$tree_path" | while read line
 do
     echo $line | spr_neighbors --nni >> $temp_file
 done
 
-nw_order $temp_file | sort -R | uniq
+cat $temp_file | nw_reroot - $root_taxon | nw_order - | sort -R | uniq
