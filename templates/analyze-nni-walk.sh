d9ec131c5ada79413e416e386690d5af400059ba
Chris Jennings-Shaffer <chrisjenningsshaffer@gmail.com>
2022-03-14 15:05:47 -0700

    fixed old file name

--- templates/analyze-nni-walk.sh
+++ templates/analyze-nni-walk.sh
@@ -4,8 +4,8 @@ set -eu
 
 nni_rep_path=ds{{ds_number}}.nni-walk.representations.csv
 cred_rep_path=ds{{ds_number}}.credible.representations.csv
-pp_rep_path=ds{{ds_number}}.pp.representations.csv
-pp_values_path=ds{{ds_number}}.pp_values.csv
+pp_rep_path=ds{{ds_number}}.mb-trees.representations.csv
+pp_values_path=ds{{ds_number}}.mb-pp.csv
 golden_pickle_path=golden/posterior.pkl
 topology_sequence_path=analysis/mb/rerooted-topology-sequence.tab
 wtch-investigate-nni-walk.py $nni_rep_path $cred_rep_path $pp_rep_path $pp_values_path $golden_pickle_path $topology_sequence_path
