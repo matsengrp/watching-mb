f997d0dca2644332bf58eb6a66d399446ac0f7ad
Chris Jennings-Shaffer <chrisjenningsshaffer@gmail.com>
2022-06-02 16:25:31 -0700

    User can now specify to use parsimony scores

--- templates/construct-nni-walk.sh
+++ templates/construct-nni-walk.sh
@@ -2,12 +2,14 @@ set -eu
 
 # This expects to run at watching-mb/nni-analysis/ds{{ds_number}}.
 sdag_rep_path=ds{{ds_number}}.representations.csv
+nwk_path=ds{{ds_number}}.topologies.nwk
+fasta_path=ds{{ds_number}}.fasta
 output_path=ds{{ds_number}}.nni-walk.representations.csv
 extra_trees_path=ds{{ds_number}}.extra-trees.representations.csv
 if [[ -f $extra_trees_path && -s $extra_trees_path ]]
 then
-  wtch-nni-likelihood-walk.py $sdag_rep_path $output_path --extra_trees_path=$extra_trees_path --max_tree_ratio=0.01
+  wtch-nni-likelihood-walk.py $sdag_rep_path $nwk_path $fasta_path $output_path --extra_trees_path=$extra_trees_path --max_tree_ratio=0.01 $1
 else
-  wtch-nni-likelihood-walk.py $sdag_rep_path $output_path --max_tree_ratio=0.01
+  wtch-nni-likelihood-walk.py $sdag_rep_path $nwk_path $fasta_path $output_path --max_tree_ratio=0.01 $1
 fi
 # Decrease the ratio for a faster run.
